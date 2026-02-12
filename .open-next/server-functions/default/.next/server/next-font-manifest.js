4e731be8f71f454349a1f5f0fe9384973edefdea
worketyamo-saa <worketyamo.dev@gmail.com>
2026-02-12 19:38:05 +0100

    fixing wrangler error

--- .open-next/server-functions/default/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/apple/Projects/menkobot/app/layout":["static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
